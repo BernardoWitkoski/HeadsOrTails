108981e3453c6e3da87d19b630e3500b9af75802
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-05 10:59:43 -0300

    Main class refactoring into home file, creating home view

---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(97, 189, 140, 1),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/logoC.png"),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: null,
+                    child: Image.asset("images/botao_jogar.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:heads_tails/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Main(),
+    home: Home(),
     debugShowCheckedModeBanner: false,
   ));
-}
-
-class Main extends StatefulWidget {
-  const Main({Key? key}) : super(key: key);
-
-  @override
-  State<Main> createState() => _MainState();
-}
-
-class _MainState extends State<Main> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
-  }
 }
